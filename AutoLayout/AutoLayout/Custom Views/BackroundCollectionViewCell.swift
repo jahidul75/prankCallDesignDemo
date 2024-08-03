75120b8aad4e323385ad19207a60f56848eff4ea
Jahidul Islam <165173540+jahidul75@users.noreply.github.com>
2024-08-03 13:44:14 +0600

    Add files via upload

---
+++ AutoLayout/AutoLayout/Custom Views/BackroundCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  BackroundCollectionViewCell.swift
+//  AutoLayout
+//
+//  Created by jahidul islam on 1/8/24.
+//
+
+import UIKit
+
+class BackroundCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgBackground: UIImageView!
+    @IBOutlet weak var imgShadow: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
